877d15dde906dc3680b17768a98ae57757d7b306
ShantanuNair <nairs@purdue.edu>
2015-02-28 03:04:31 -0500

    Added POST requests
    Sends battery stats to heroku - untested

--- save-my-battery-master/save-my-battery.js
+++ save-my-battery-master/save-my-battery.js
@@ -6,11 +6,13 @@ var terminalCommand = 'ioreg -n AppleSmartBattery -r';
 var seconds = 3
 var frequency = seconds * 1000;
 
+var pc = 0;
+
 var onTerminalOutput = function(error, stdout) {
     if (!stdout) return;
     var max = /"MaxCapacity" = (\d+)/m.exec(stdout)[1];
     var current = /"CurrentCapacity" = (\d+)/m.exec(stdout)[1];
-    var pc = Math.round(current / max * 100);
+    pc = Math.round(current / max * 100);
     var isCharging = /"IsCharging" = Yes/.test(stdout);
 
     sendMessage(pc + '% Battery Left');
@@ -19,18 +21,6 @@ var onTerminalOutput = function(error, stdout) {
 var getBatteryPC = function(){
     exec(terminalCommand, onTerminalOutput);
     setTimeout(getBatteryPC, frequency);
-};
-
-var sendMessage = function(message) {
-    req.write(userString);
-    req.end();
-    // console.log(message);
-    // notifier.notify({
-    //     title: '🔋 Battery Alert',
-    //     message: message,
-    //     group: 'balert',
-    //     sender: 'balert'
-    // });
 };
 
 
@@ -66,6 +56,21 @@ var req = http.request(options, function(res) {
     var resultObject = JSON.parse(responseString);
   });
 });
+
+
+
+var sendMessage = function(message) {
+    req.write(userString);
+    req.end();
+    // console.log(message);
+    // notifier.notify({
+    //     title: '🔋 Battery Alert',
+    //     message: message,
+    //     group: 'balert',
+    //     sender: 'balert'
+    // });
+};
+
 
 
 
